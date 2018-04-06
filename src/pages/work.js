7ed069ed1c70c8ce8550dce99c2a89df604134c2
Joe DePumpo <joe@depumpo.com>
2018-04-05 20:37:55 -0400

    updated work page, need to fix resume link

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Container from '../components/Container'
+import containerStyle from '../components/Container/container.module.css'
 
 const Work = () => (
   <div>
-    <span className='pageTitle'>Experience</span>
-    <p>Building as we speak!</p>
+ <Container>
+ <span className="pageTitle">Experience</span>
+</Container>
+<Container className={containerStyle.blue + ' ' + containerStyle.whiteText + ' ' + containerStyle.containerRounded}>
+       <p>You can find a <Link to="#">copy of my resume</Link> here (PDF).</p>
+  </Container>
   </div>
 )
 
